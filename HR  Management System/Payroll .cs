95efb0de2cf0f7b22952e57e36ca0abae8c21f15
milakwa <malakalamir55@gmail.com>
2025-09-06 09:44:50 +0300

    Update Payroll

--- HR  Management System/Payroll .cs
+++ HR  Management System/Payroll .cs
@@ -1,22 +1,49 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HR__Management_System
-{
+{  
+    /*public class Payroll
+   {
+      public bool CalculateSalary(Employee emp, double hoursWorked, out decimal totalPay)
+      {
+        totalPay = emp.CalculatePay(hoursWorked);
+
+        if (totalPay == 0)
+        {
+            Console.WriteLine($"❌ Error: Failed to calculate salary for Employee ID={emp.Id}. Check hours worked.");
+            Logger.WriteLog("PAYROLL", $"Failed salary calculation for {emp.Name} (ID={emp.Id})");
+            return false; // فشل
+        }
+
+        string role = emp.GetType().Name;
+        Logger.WriteLog("PAYROLL",
+            $"Calculated salary for {role} [ID={emp.Id}, Name={emp.Name}]: {totalPay:C} for {hoursWorked} hours worked.");
+        return true; // نجاح
+      }
+    }*/
+
     public class Payroll
     {
-        public Logger logger;
-        public Payroll(Logger logger)
-        {
-            this.logger = logger;
-        }   
         public decimal CalculateSalary(Employee emp,double hoursWorked)
         {
-            decimal totalPay = emp.CalculatePay(hoursWorked);
-            logger.WriteLog($"Calculated salary for Employee [ID={emp.Id}, Name={emp.Name}]: {totalPay:C} for {hoursWorked} hours worked.");
+            decimal totalPay = emp.CalculatePay(hoursWorked); 
+
+            if (totalPay == 0)
+            {
+                Console.WriteLine($"Could not calculate salary for {emp.Name} (ID={emp.Id}). Please check hours worked.");
+                Logger.WriteLog("PAYROLL", $"Failed salary calculation for {emp.Name} (ID={emp.Id})");
+                return 0;
+            }
+
+            string role = emp.GetType().Name;
+
+            Logger.WriteLog("PAYROLL",$"Calculated salary for {role} [ID={emp.Id}, Name={emp.Name}]: {totalPay:C} for {hoursWorked} hours worked.");
+            Console.WriteLine($"Salary for {emp.Name} ({role}) = {totalPay:C}");
+
             return totalPay;
             
         }
