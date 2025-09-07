d4b895556fa4e191e4d3043cb47e8a82a7f99be8
milakwa <malakalamir55@gmail.com>
2025-09-07 03:56:43 +0300

    Update LeaveRequest

--- HR  Management System/LeaveRequest.cs
+++ HR  Management System/LeaveRequest.cs
@@ -8,11 +8,12 @@ namespace HR__Management_System
 {
     public class LeaveRequest
     {
+        // Properties of a leave request
        public int EmpId { get; set; }
        public int Days { get; set; }
        public string Reason { get; set; }
        public string Status { get; set; }
-
+        // Constructor
         public LeaveRequest(int id,int days,string reason)
         {
             EmpId = id;
@@ -23,6 +24,7 @@ namespace HR__Management_System
             Logger.WriteLog("LEAVE", $"New leave request created for EmpId={EmpId}, Days={Days}, Reason={Reason}, Status={Status}");
         
         }
+        // Methods to approve or reject the leave request
         public void Approve()
         {
             Status = "Approved";
