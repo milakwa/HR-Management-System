27eb14a7f287a23d19cf9f1b772b417af5ec12f4
milakwa <malakalamir55@gmail.com>
2025-09-07 03:29:19 +0300

    Add files via upload

---
+++ HR  Management System/Department.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR__Management_System
+{
+    // Enum for different departments
+    public enum Department
+    {
+        HR = 1,
+        IT,
+        Finance,
+        Sales,
+        Marketing,
+        Operations,
+        CustomerSupport,
+        RnD,
+        Legal,
+        Admin,
+        Procurement,
+        Logistics,
+        Production,
+        QualityAssurance,
+        Training,
+        PublicRelations,
+        BusinessDevelopment,
+        Strategy,
+        Security,
+        Maintenance
+    }
+}
