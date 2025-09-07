123049ed5104765297b5baff3368ed56c842f6d0
milakwa <malakalamir55@gmail.com>
2025-09-07 03:54:29 +0300

    Update Intern

--- HR  Management System/Intern .cs
+++ HR  Management System/Intern .cs
@@ -6,18 +6,19 @@ using System.Threading.Tasks;
 
 namespace HR__Management_System
 {
+    // Intern class inheriting from Employee
     public class Intern : Employee
     {
-        public Intern(int id, string name, string department, decimal salary) 
-            : base(id, name, department, salary )
+        public Intern(int id, string name, Department department, decimal salary)
+         : base(id, name, department, salary)
         {
         }
-
+        // Interns have a fixed stipend, no overtime
         public override decimal CalculatePay(double hoursWorked)
         {
             if (hoursWorked < 0)
             {
-                Console.WriteLine("Hours worked cannot be negative!");
+                Console.WriteLine("\nHours worked cannot be negative!");
                 Logger.WriteLog("PAYROLL", $"Failed to calculate pay for {Name} (ID={Id})");
                 return 0;
             }
