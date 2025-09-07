0c8eef57054af7b880f7c50aa81a5715e1989f8d
milakwa <malakalamir55@gmail.com>
2025-09-07 03:56:04 +0300

    Update Payroll

--- HR  Management System/Payroll .cs
+++ HR  Management System/Payroll .cs
@@ -5,40 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace HR__Management_System
-{  
-    /*public class Payroll
-   {
-      public bool CalculateSalary(Employee emp, double hoursWorked, out decimal totalPay)
-      {
-        totalPay = emp.CalculatePay(hoursWorked);
-
-        if (totalPay == 0)
-        {
-            Console.WriteLine($"❌ Error: Failed to calculate salary for Employee ID={emp.Id}. Check hours worked.");
-            Logger.WriteLog("PAYROLL", $"Failed salary calculation for {emp.Name} (ID={emp.Id})");
-            return false; // فشل
-        }
-
-        string role = emp.GetType().Name;
-        Logger.WriteLog("PAYROLL",
-            $"Calculated salary for {role} [ID={emp.Id}, Name={emp.Name}]: {totalPay:C} for {hoursWorked} hours worked.");
-        return true; // نجاح
-      }
-    }*/
-
+{
     public class Payroll
     {
+        // Calculate salary based on hours worked
         public decimal CalculateSalary(Employee emp,double hoursWorked)
         {
             decimal totalPay = emp.CalculatePay(hoursWorked); 
 
             if (totalPay == 0)
             {
-                Console.WriteLine($"Could not calculate salary for {emp.Name} (ID={emp.Id}). Please check hours worked.");
+                Console.WriteLine($"\nCould not calculate salary for {emp.Name} (ID={emp.Id}). Please check hours worked.\n");
                 Logger.WriteLog("PAYROLL", $"Failed salary calculation for {emp.Name} (ID={emp.Id})");
                 return 0;
             }
 
+            // Log the payroll calculation
             string role = emp.GetType().Name;
 
             Logger.WriteLog("PAYROLL",$"Calculated salary for {role} [ID={emp.Id}, Name={emp.Name}]: {totalPay:C} for {hoursWorked} hours worked.");
